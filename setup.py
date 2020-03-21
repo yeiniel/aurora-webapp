384865f3f0f3c93aa4ff51718593da033f49c3fa
Yeiniel Suarez Sosa <yeiniel@gmail.com>
2020-03-21 17:07:56 -0500

    chore: update setup.py to use aurora_webapp/__init__ as source of truth for package version

--- setup.py
+++ setup.py
@@ -29,14 +29,34 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
+import codecs
+import os
+import re
 import setuptools
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+def read(*parts):
+    with codecs.open(os.path.join(here, *parts), 'r') as fp:
+        return fp.read()
+
+
+def find_version(*file_paths):
+    version_file = read(*file_paths)
+    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                              version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError("Unable to find version string.")
+
+
 setuptools.setup(
     name="aurora-webapp",  # Replace with your own username
-    version="0.0.1",
+    version=find_version("aurora_webapp", "__init__.py"),
     author="Yeiniel Suarez Sosa",
     author_email="yeiniel@gmail.com",
     description="Aurora Web Application Framework",
